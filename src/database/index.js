72302d38d95675c0c1ec8fc21f4d74c6dfd2d7ca
Cassius Sanches Gachido de Souza <cassius.sanches@hotmail.com>
2019-10-09 10:22:26 -0300

    Adding authentication and creating UserController

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 import Sequelize from 'sequelize';
 
+import User from '../app/models/User';
+
 import databaseConfig from '../config/database';
 
-const models = [];
+const models = [User];
+
 class Database {
   constructor() {
     this.init();
@@ -10,7 +13,6 @@ class Database {
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-
     models.map(model => model.init(this.connection));
   }
 }
